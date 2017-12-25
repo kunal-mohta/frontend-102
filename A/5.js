331274a9e967603c66aa155b7c8e5e971508d5cc
kunal-mohta <kunalmohta1818@gmail.com>
2017-12-25 22:32:41 +0530

    AT5

---
+++ A/5.js
@@ -0,0 +1,11 @@
+module.exports = function foo(a){
+	return new Promise( (resolve,reject) => {
+		setTimeout( () => {
+			if((typeof a) == "string") resolve("true");
+			else resolve("false");
+		}, 1000);
+	})
+	.then(
+		result => console.log("Promise resolved to "+result)
+	);
+}
