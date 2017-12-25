72edb9e34f01fb74b30fb2a10f757ef29e6742e1
kunal-mohta <kunalmohta1818@gmail.com>
2017-12-25 23:03:40 +0530

    AT5

--- A/5.js
+++ A/5.js
@@ -1,8 +1,8 @@
-module.exports = function foo(a){
+module.exports = (a) => {
 	return new Promise( (resolve,reject) => {
 		setTimeout( () => {
-			if((typeof a) == "string") resolve("true");
-			else resolve("false");
+			if((typeof a) == "string") resolve(true);
+			else resolve(false);
 		}, 1000);
 	})
 	.then(
